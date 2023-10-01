7fd8642840c44b1342c62674a2f4bc2caac226fc
Chris Duncan <veqryn@hotmail.com>
2023-10-01 00:56:13 -0600

    Refactor and pull helper methods to own files

---
+++ helpers.go
@@ -0,0 +1,172 @@
+package dedup
+
+import (
+	"fmt"
+	"log/slog"
+	"strings"
+
+	"modernc.org/b/v2"
+)
+
+// TODO: also create a sorting middleware as well
+// TODO: also create a pretty json printer that still prints to only 1 line, just prettier
+
+// getKeyClosure returns a function to be used to resolve a key at the root level, determining its behavior when it
+// would otherwise conflict/duplicate the 4 built-in attribute keys (time, level, msg, source).
+func getKeyClosure(resolveBuiltinKeyConflict func(k string) (string, bool)) func(key string, depth int) (string, bool) {
+	return func(key string, depth int) (string, bool) {
+		if depth == 0 {
+			return resolveBuiltinKeyConflict(key)
+		}
+		return key, true
+	}
+}
+
+// IncrementIfBuiltinKeyConflict will, if there is a conflict/duplication at the root level (not in a group) with one of
+// the built-in keys, add "#01" to the end of the key
+func IncrementIfBuiltinKeyConflict(key string) (string, bool) {
+	if DoesBuiltinKeyConflict(key) {
+		return IncrementKeyName(key, 1), true // Don't overwrite the built-in attribute keys
+	}
+	return key, true
+}
+
+// DropIfBuiltinKeyConflict will, if there is a conflict/duplication at the root level (not in a group) with one of the
+// built-in keys, drop the whole attribute
+func DropIfBuiltinKeyConflict(key string) (string, bool) {
+	if DoesBuiltinKeyConflict(key) {
+		return "", false // Drop the attribute
+	}
+	return key, true
+}
+
+// KeepIfBuiltinKeyConflict will keep all keys even if there would be a conflict/duplication at the root level (not in a
+// group) with one of the built-in keys
+func KeepIfBuiltinKeyConflict(key string) (string, bool) {
+	return key, true // Keep all
+}
+
+// DoesBuiltinKeyConflict returns true if the key conflicts with the builtin keys.
+// This will only be called on all root level (not in a group) attribute keys.
+func DoesBuiltinKeyConflict(key string) bool {
+	if key == slog.TimeKey || key == slog.LevelKey || key == slog.MessageKey || key == slog.SourceKey {
+		return true
+	}
+	return false
+}
+
+// IncrementKeyName adds a count onto the key name after the first seen.
+// Example: keyname, keyname#01, keyname#02, keyname#03
+func IncrementKeyName(key string, index int) string {
+	if index == 0 {
+		return key
+	}
+	return fmt.Sprintf("%s#%02d", key, index)
+}
+
+// CaseSensitiveCmp is a case-sensitive comparison and ordering function that orders by byte values
+func CaseSensitiveCmp(a, b string) int {
+	if a == b {
+		return 0
+	}
+	if a > b {
+		return 1
+	}
+	return -1
+}
+
+// CaseInsensitiveCmp is a case-insensitive comparison and ordering function that orders by byte values
+func CaseInsensitiveCmp(a, b string) int {
+	a = strings.ToLower(a)
+	b = strings.ToLower(b)
+	if a == b {
+		return 0
+	}
+	if a > b {
+		return 1
+	}
+	return -1
+}
+
+// buildAttrs converts the deduplicated map back into an attribute array, with any subtrees converted into slog.Group's
+func buildAttrs(uniq *b.Tree[string, any]) []slog.Attr {
+	en, err := uniq.SeekFirst()
+	if err != nil {
+		return nil // Empty (btree only returns an error when empty)
+	}
+	defer en.Close()
+
+	// Iterate through all values in the map, add to slice
+	attrs := make([]slog.Attr, 0, uniq.Len())
+	for k, i, err := en.Next(); err == nil; k, i, err = en.Next() {
+		// Values will either be an attribute, or a subtree
+		switch v := i.(type) {
+		case slog.Attr:
+			attrs = append(attrs, v)
+		case *b.Tree[string, any]:
+			// Convert subtree into a group
+			attrs = append(attrs, slog.Attr{Key: k, Value: slog.GroupValue(buildAttrs(v)...)})
+		default:
+			panic("unexpected type in attribute map")
+		}
+	}
+	return attrs
+}
+
+// groupOrAttrs holds either a group name or a list of slog.Attrs.
+// It also holds a reference/link to its parent groupOrAttrs, forming a linked list.
+type groupOrAttrs struct {
+	group string        // group name if non-empty
+	attrs []slog.Attr   // attrs if non-empty
+	next  *groupOrAttrs // parent
+}
+
+// WithGroup returns a new groupOrAttrs that includes the given group, and links to the old groupOrAttrs.
+// Safe to call on a nil groupOrAttrs.
+func (g *groupOrAttrs) WithGroup(name string) *groupOrAttrs {
+	// Empty-name groups are inlined as if they didn't exist
+	if name == "" {
+		return g
+	}
+	return &groupOrAttrs{
+		group: name,
+		next:  g,
+	}
+}
+
+// WithAttrs returns a new groupOrAttrs that includes the given attrs, and links to the old groupOrAttrs.
+// Safe to call on a nil groupOrAttrs.
+func (g *groupOrAttrs) WithAttrs(attrs []slog.Attr) *groupOrAttrs {
+	if len(attrs) == 0 {
+		return g
+	}
+	return &groupOrAttrs{
+		attrs: attrs,
+		next:  g,
+	}
+}
+
+// collectGroupOrAttrs unrolls all individual groupOrAttrs and collects them into a slice, ordered from oldest to newest
+func collectGroupOrAttrs(gs ...*groupOrAttrs) []*groupOrAttrs {
+	// Get a total count of all groups in the group linked-list chain
+	n := 0
+	for _, g := range gs {
+		for ga := g; ga != nil; ga = ga.next {
+			n++
+		}
+	}
+
+	// The groupOrAttrs on the handler is a linked list starting from the newest to the oldest set of attributes/groups.
+	// Within each groupOrAttrs, all attributes are in a slice that is ordered from oldest to newest.
+	// To make things consistent we will reverse the order of the groupOrAttrs, so that it goes from oldest to newest,
+	// thereby matching the order of the attributes.
+	res := make([]*groupOrAttrs, n)
+	j := 0
+	for i := len(gs) - 1; i >= 0; i-- {
+		for ga := gs[i]; ga != nil; ga = ga.next {
+			res[len(res)-j-1] = ga
+			j++
+		}
+	}
+	return res
+}
--- overwrite_handler.go
+++ overwrite_handler.go
@@ -3,34 +3,11 @@ package dedup
 import (
 	"context"
 	"log/slog"
-	"strings"
 
 	"modernc.org/b/v2"
 )
 
-// TODO: also create an append version, and a key name increment version
-// TODO: also create a sorting middleware as well
-// TODO: also create a pretty json printer that still prints to only 1 line, just prettier
-
-var (
-	// TimeKey is the key used by the built-in handlers for the time when the log method is called.
-	// Set before instantiating any handlers.
-	TimeKey = slog.TimeKey
-
-	// LevelKey is the key used by the built-in handlers for the level of the log call.
-	// Set before instantiating any handlers.
-	LevelKey = slog.LevelKey
-
-	// MessageKey is the key used by the built-in handlers for the message of the log call.
-	// Set before instantiating any handlers.
-	MessageKey = slog.MessageKey
-
-	// SourceKey is the key used by the built-in handlers for the source file and line of the log call.
-	// Set before instantiating any handlers.
-	SourceKey = slog.SourceKey
-)
-
-// OverwriteHandlerOptions are options for a OverwriteHandler. An empty options struct is NOT valid.
+// OverwriteHandlerOptions are options for a OverwriteHandler
 type OverwriteHandlerOptions struct {
 	// Comparison function to determine if two keys are equal
 	KeyCompare func(a, b string) int
@@ -75,7 +52,7 @@ func NewOverwriteHandler(next slog.Handler, opts *OverwriteHandlerOptions) *Over
 	}
 }
 
-// Enabled reports whether the handler handles records at the given level.
+// Enabled reports whether the next handler handles records at the given level.
 // The handler ignores records whose level is lower.
 func (h *OverwriteHandler) Enabled(ctx context.Context, level slog.Level) bool {
 	return h.next.Enabled(ctx, level)
@@ -94,7 +71,7 @@ func (h *OverwriteHandler) Handle(ctx context.Context, r slog.Record) error {
 
 	// Resolve groups and with-attributes
 	uniq := b.TreeNew[string, any](h.keyCompare)
-	createAttrTree(uniq, goas, 0, h.keyCompare, h.getKey)
+	h.createAttrTree(uniq, goas, 0)
 
 	// Add all attributes to new record (because old record has all the old attributes)
 	newR := &slog.Record{
@@ -126,16 +103,16 @@ func (h *OverwriteHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
 
 // createAttrTree recursively goes through all groupOrAttrs, resolving their attributes and creating subtrees as
 // necessary, adding the results to the map
-func createAttrTree(uniq *b.Tree[string, any], goas []*groupOrAttrs, depth int, keyCompare func(a, b string) int, getKey func(key string, depth int) (string, bool)) {
+func (h *OverwriteHandler) createAttrTree(uniq *b.Tree[string, any], goas []*groupOrAttrs, depth int) {
 	if len(goas) == 0 {
 		return
 	}
 
 	// If a group is encountered, create a subtree for that group and all groupOrAttrs after it
 	if goas[0].group != "" {
-		if key, ok := getKey(goas[0].group, depth); ok {
-			uniqGroup := b.TreeNew[string, any](keyCompare)
-			createAttrTree(uniqGroup, goas[1:], depth+1, keyCompare, getKey)
+		if key, ok := h.getKey(goas[0].group, depth); ok {
+			uniqGroup := b.TreeNew[string, any](h.keyCompare)
+			h.createAttrTree(uniqGroup, goas[1:], depth+1)
 			// Ignore empty groups, otherwise put subtree into the map
 			if uniqGroup.Len() > 0 {
 				uniq.Set(key, uniqGroup)
@@ -145,14 +122,15 @@ func createAttrTree(uniq *b.Tree[string, any], goas []*groupOrAttrs, depth int, 
 	}
 
 	// Otherwise, set all attributes for this groupOrAttrs, and then call again for remaining groupOrAttrs's
-	resolveValues(uniq, goas[0].attrs, depth, keyCompare, getKey)
-	createAttrTree(uniq, goas[1:], depth, keyCompare, getKey)
+	h.resolveValues(uniq, goas[0].attrs, depth)
+	h.createAttrTree(uniq, goas[1:], depth)
 }
 
 // resolveValues iterates through the attributes, resolving them and putting them into the map.
 // If a group is encountered (as an attribute), it will be separately resolved and added as a subtree.
 // Since attributes are ordered from oldest to newest, it overwrites keys as it goes.
-func resolveValues(uniq *b.Tree[string, any], attrs []slog.Attr, depth int, keyCompare func(a, b string) int, getKey func(key string, depth int) (string, bool)) {
+func (h *OverwriteHandler) resolveValues(uniq *b.Tree[string, any], attrs []slog.Attr, depth int) {
+	var ok bool
 	for _, a := range attrs {
 		a.Value = a.Value.Resolve()
 		if a.Equal(slog.Attr{}) {
@@ -160,8 +138,7 @@ func resolveValues(uniq *b.Tree[string, any], attrs []slog.Attr, depth int, keyC
 		}
 
 		// Default situation: resolve the key and put it into the map
-		var ok bool
-		a.Key, ok = getKey(a.Key, depth)
+		a.Key, ok = h.getKey(a.Key, depth)
 		if !ok {
 			continue
 		}
@@ -173,159 +150,17 @@ func resolveValues(uniq *b.Tree[string, any], attrs []slog.Attr, depth int, keyC
 
 		// Groups with empty keys are inlined
 		if a.Key == "" {
-			resolveValues(uniq, a.Value.Group(), depth, keyCompare, getKey)
+			h.resolveValues(uniq, a.Value.Group(), depth)
 			continue
 		}
 
 		// Create a subtree for this group
-		uniqGroup := b.TreeNew[string, any](keyCompare)
-		resolveValues(uniqGroup, a.Value.Group(), depth+1, keyCompare, getKey)
+		uniqGroup := b.TreeNew[string, any](h.keyCompare)
+		h.resolveValues(uniqGroup, a.Value.Group(), depth+1)
 
 		// Ignore empty groups, otherwise put subtree into the map
 		if uniqGroup.Len() > 0 {
 			uniq.Set(a.Key, uniqGroup)
 		}
 	}
-}
-
-// buildAttrs converts the deduplicated map back into an attribute array, with any subtrees converted into slog.Group's
-func buildAttrs(uniq *b.Tree[string, any]) []slog.Attr {
-	en, err := uniq.SeekFirst()
-	if err != nil {
-		return nil // Empty (btree only returns an error when empty)
-	}
-	defer en.Close()
-
-	// Iterate through all values in the map, add to slice
-	attrs := make([]slog.Attr, 0, uniq.Len())
-	for k, i, err := en.Next(); err == nil; k, i, err = en.Next() {
-		// Values will either be an attribute, or a subtree
-		switch v := i.(type) {
-		case slog.Attr:
-			attrs = append(attrs, v)
-		case *b.Tree[string, any]:
-			// Convert subtree into a group
-			attrs = append(attrs, slog.Attr{Key: k, Value: slog.GroupValue(buildAttrs(v)...)})
-		default:
-			panic("unexpected type in attribute map")
-		}
-	}
-	return attrs
-}
-
-// groupOrAttrs holds either a group name or a list of slog.Attrs.
-// It also holds a reference/link to its parent groupOrAttrs, forming a linked list.
-type groupOrAttrs struct {
-	group string        // group name if non-empty
-	attrs []slog.Attr   // attrs if non-empty
-	next  *groupOrAttrs // parent
-}
-
-// WithGroup returns a new groupOrAttrs that includes the given group, and links to the old groupOrAttrs.
-// Safe to call on a nil groupOrAttrs.
-func (g *groupOrAttrs) WithGroup(name string) *groupOrAttrs {
-	// Empty-name groups are inlined as if they didn't exist
-	if name == "" {
-		return g
-	}
-	return &groupOrAttrs{
-		group: name,
-		next:  g,
-	}
-}
-
-// WithAttrs returns a new groupOrAttrs that includes the given attrs, and links to the old groupOrAttrs.
-// Safe to call on a nil groupOrAttrs.
-func (g *groupOrAttrs) WithAttrs(attrs []slog.Attr) *groupOrAttrs {
-	if len(attrs) == 0 {
-		return g
-	}
-	return &groupOrAttrs{
-		attrs: attrs,
-		next:  g,
-	}
-}
-
-// collectGroupOrAttrs unrolls all individual groupOrAttrs and collects them into a slice, ordered from oldest to newest
-func collectGroupOrAttrs(gs ...*groupOrAttrs) []*groupOrAttrs {
-	// Get a total count of all groups in the group linked-list chain
-	n := 0
-	for _, g := range gs {
-		for ga := g; ga != nil; ga = ga.next {
-			n++
-		}
-	}
-
-	// The groupOrAttrs on the handler is a linked list starting from the newest to the oldest set of attributes/groups.
-	// Within each groupOrAttrs, all attributes are in a slice that is ordered from oldest to newest.
-	// To make things consistent we will reverse the order of the groupOrAttrs, so that it goes from oldest to newest,
-	// thereby matching the order of the attributes.
-	res := make([]*groupOrAttrs, n)
-	j := 0
-	for i := len(gs) - 1; i >= 0; i-- {
-		for ga := gs[i]; ga != nil; ga = ga.next {
-			res[len(res)-j-1] = ga
-			j++
-		}
-	}
-	return res
-}
-
-// getKeyClosure returns a function to be used to resolve a key at the root level, determining its behavior when it
-// would otherwise conflict/duplicate the 4 built-in attribute keys (time, level, msg, source).
-func getKeyClosure(resolveBuiltinKeyConflict func(k string) (string, bool)) func(key string, depth int) (string, bool) {
-	return func(key string, depth int) (string, bool) {
-		if depth == 0 {
-			return resolveBuiltinKeyConflict(key)
-		}
-		return key, true
-	}
-}
-
-// IncrementIfBuiltinKeyConflict will, if there is a conflict/duplication at the root level (not in a group) with one of
-// the built-in keys, add "#01" to the end of the key
-func IncrementIfBuiltinKeyConflict(key string) (string, bool) {
-	if key == TimeKey || key == LevelKey || key == MessageKey || key == SourceKey {
-		return key + "#01", true // Don't overwrite the built-in attribute keys
-	}
-	return key, true
-}
-
-// DropIfBuiltinKeyConflict will, if there is a conflict/duplication at the root level (not in a group) with one of the
-// built-in keys, drop the whole attribute
-func DropIfBuiltinKeyConflict(key string) (string, bool) {
-	if key == TimeKey || key == LevelKey || key == MessageKey || key == SourceKey {
-		return "", false // Drop the attribute
-	}
-	return key, true
-}
-
-// KeepIfBuiltinKeyConflict will keep all keys even if there would be a conflict/duplication at the root level (not in a
-// group) with one of the built-in keys
-func KeepIfBuiltinKeyConflict(key string) (string, bool) {
-	return key, true // Keep all
-}
-
-// CaseSensitiveCmp is a case-sensitive comparison and ordering function that orders by byte values
-func CaseSensitiveCmp(a, b string) int {
-	if a == b {
-		return 0
-	}
-	if a > b {
-		return 1
-	}
-	return -1
-}
-
-// CaseInsensitiveCmp is a case-insensitive comparison and ordering function that orders by byte values
-func CaseInsensitiveCmp(a, b string) int {
-	a = strings.ToLower(a)
-	b = strings.ToLower(b)
-	if a == b {
-		return 0
-	}
-	if a > b {
-		return 1
-	}
-	return -1
 }
